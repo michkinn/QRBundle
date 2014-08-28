8cedcde172c4cb127562d7b0a1f3261133c537a6
Gregory SCHURGAST <gschurgast@vente-unique.com>
2014-08-28 18:45:46 +0200

    Init

---
+++ QRBundle.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gsch
+ * Date: 28/08/2014
+ * Time: 18:31
+ */
+
+namespace Negko\QRBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class QRBundle {
+
+} 
